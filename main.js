55dc9581cd3e70c012566bbce8abc70692d4a24c
Aaron Bagaoisan <aaronbagaoisan123@gmail.com>
2019-12-01 19:27:01 -0800

    main.js

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-const seven = 7;
+console.log(27 + 27);
+console.log(1 + 2 + 3 + 4 + 5 + 6);
+console.log((4 + 6 + 9) / 77);
+let a = 10;
+console.log((9 * a));
 
-console.log(seven);
+let b = 7 * a;
+console.log(b);
+
+const max = 57;
+
+const actual = max - 13;
+
+const percentage = actual/max;
+
+console.log(percentage);
